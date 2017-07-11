208cb4f48ddb1bcbef05f23b1905498bfd57efe9
Garrett Kucinski <garrettryan@doejo.com>
2017-07-11 15:15:57 -0500

    reinstated sass

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-// const sassMiddleware = require('node-sass-middleware');
+const sassMiddleware = require('node-sass-middleware');
 const path = require('path');
 
 const homeRoute = require('./routes');
@@ -20,14 +20,14 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// app.use(sassMiddleware({
-//     src: path.join(__dirname, '/scss'),
-//     dest: path.join(__dirname, '/public/stylesheets'),
-//     indentedSyntax: false,
-//     sourceMap: true,
-//     outputStyle: 'compressed',
-//     prefix: '/static/stylesheets'
-// }));
+app.use(sassMiddleware({
+    src: path.join(__dirname, '/scss'),
+    dest: path.join(__dirname, '/public/stylesheets'),
+    indentedSyntax: false,
+    sourceMap: true,
+    outputStyle: 'compressed',
+    prefix: '/static/stylesheets'
+}));
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
